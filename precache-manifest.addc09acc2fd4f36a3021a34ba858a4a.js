920d1f438b5ff9ebd7d9c5bcf0d01675865340c5
Paul Rogov <1624921+pvrogov@users.noreply.github.com>
2020-10-05 21:39:41 +0300

    Updates

---
+++ precache-manifest.addc09acc2fd4f36a3021a34ba858a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa5243d72ec997bb5931991c1e6b016",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "f6d1b54657c80c9b790d",
+    "url": "/react-todo-app/static/css/main.11f30128.chunk.css"
+  },
+  {
+    "revision": "fe2a9a2c6ddc00270ce6",
+    "url": "/react-todo-app/static/js/2.ec4a3c2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.ec4a3c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d1b54657c80c9b790d",
+    "url": "/react-todo-app/static/js/main.c97f9824.chunk.js"
+  },
+  {
+    "revision": "7f77db63e75cca65f84c",
+    "url": "/react-todo-app/static/js/runtime-main.2243a9af.js"
+  }
+]);
